4d5bb85bfda948c9905101e309abd30ce003af28
ryanwlee <ryan_lee2@intuit.com>
2022-06-21 12:56:44 -0600

    fix: added best wetsern

---
+++ bestwestern/precache-manifest.d13f0233177aced937cc90aee0f02885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046752c353396f15dafacd1e89087f33",
+    "url": "/bestwestern/index.html"
+  },
+  {
+    "revision": "5f66ceebb30ce3c5b6ed",
+    "url": "/bestwestern/static/css/2.06fce332.chunk.css"
+  },
+  {
+    "revision": "7c3dda990da2b7390675",
+    "url": "/bestwestern/static/css/main.28d9c1c7.chunk.css"
+  },
+  {
+    "revision": "5f66ceebb30ce3c5b6ed",
+    "url": "/bestwestern/static/js/2.79e42ba4.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/bestwestern/static/js/2.79e42ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3dda990da2b7390675",
+    "url": "/bestwestern/static/js/main.13b82c42.chunk.js"
+  },
+  {
+    "revision": "97b220db1f5fb43d7ec7",
+    "url": "/bestwestern/static/js/runtime-main.93a149a3.js"
+  }
+]);
